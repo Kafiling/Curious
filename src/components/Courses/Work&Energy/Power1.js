869d91d64ceb9ad720b21ee01ee5262957c41a00
bbbbbbbbbbbbbe <tharawaranuset@gmail.com>
2021-12-26 14:10:34 +0700

    s

--- src/components/Courses/Work&Energy/Power1.js
+++ src/components/Courses/Work&Energy/Power1.js
@@ -7,7 +7,7 @@ import {Scene} from './Material/Work4Scene1';
 import {AuthContext, db} from 'Firebase'
 
 
-export default function dPower1() {
+export default function Power1() {
 // Set Page
 var [page, setPage] = useState(1);
 //Var Answered
