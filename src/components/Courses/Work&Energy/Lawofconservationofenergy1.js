20b6ec12b0b0a9143c5253f6f751417149e87dc3
Kafiling <kafilkafilth@gmail.com>
2022-01-22 21:37:28 +0700

    gg

--- src/components/Courses/Work&Energy/Lawofconservationofenergy1.js
+++ src/components/Courses/Work&Energy/Lawofconservationofenergy1.js
@@ -5,6 +5,7 @@ import {Scene} from './Material/Work4Scene1';
 import {CorrectAlert, IncorrectAlert, UpvoteAlert, ReportAlert} from './Alert'
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
+import {Scene as Scene1} from './Material/LOCE1Scene1';
 //ประกาศตัวแปรของ Firebase Service
 import {AuthContext, db} from 'Firebase'
 
@@ -169,7 +170,11 @@ return(
 เราพบว่าพลังงานในรูปหนึ่งสามารถเปลี่ยนเป็นพลังงานรูปอื่นๆได้และผลรวมพลังงาน<br/>ในวัตถุจะมีค่าคงที่เสมอ 
 เช่น เวลาปล่อยวัตถุจากที่สูง เริ่มแรกวัตถุจะไม่มีความเร็ว แต่เมื่อวัตถุกำลังตกลง 
 พลังงานศักย์จะถูกเปลี่ยนไปเป็นพลังงานจลน์ทำให้เกิดความเร็วขึ้น โดยเราเรียก<br/>แรงที่กระทำแล้วทำให้พลังงานกล(พลังงานศักย์และพลังงานจลน์)ไม่เปลี่ยนว่า แรงอนุรักษ์
-<br/>
+</div>
+<div className='SceneContainer'>
+      <Scene1/>
+    </div>
+<div className="LabInfo">
 โดย พลังงานที่ 1 = พลังงานที่ 2<br/><MathJaxContext>
       <MathJax>\[E_1 = E_2\]</MathJax>
       </MathJaxContext>
@@ -183,7 +188,7 @@ return(
 </div>
 
 <div className="split QuestionAnswer"> 
-  <div className="LabNumber">Introducing Work</div>
+  <div className="LabNumber">Energy Transformed</div>
   <div className="ProgessBar"><progress value="0" max="100"></progress></div>
   <div className="Question">กดปุ่มสีเขียว เพื่อไปหน้าต่อไป</div>
   <div className="AnswerList">
@@ -202,10 +207,8 @@ return(
 <div className="LabName">กฎการอนุรักษ์พลังงาน</div>
 <div div className="LabInfo">ด้านล่างเป็นตัวอย่างเพื่อเพิ่มความเข้าใจครับ<br/><br/>
 จากรูปวัตถุมีมวล 1 kg  เคลื่อนที่จากจุด A ไป จุด E โดยทางพื้นผิวไม่มีแรงเสียดทาน จงหาความเร็ววัตถุที่จุด C และ D
-
-
-<br/><br/><br/>********รูปรูปรูปรูปรูปรูป*<br/><br/><br/>
-1) สามารถหาความเร็วที่จุด C เพราะพลังงานที่จุด A = พลังงานที่จุด C<br/>
+<img className='LabImg' id='img' alt ="LabImg"src="https://firebasestorage.googleapis.com/v0/b/keep-curious.appspot.com/o/Work%26Energy%2FEp1%2FEp1P5.png?alt=media&token=44b39353-448b-4eb0-8655-3277af1ecc80" />
+1) สามารถหาความเร็วที่จุด C จากพลังงานรวมที่จุด A = พลังงานรวมที่จุด C<br/>
 เราจะพบว่าที่จุด A มีพลังงานศักย์โน้มถ่วง (มีความสูง) และจุด C มีทั้งพลังงานศักย์โน้มถ่วง (มีความสูง) และ พลังงานจลน์ (มีความเร็ว)<br/>
 <MathJaxContext>
       <MathJax>\[(m \cdot g \cdot h)_A = (m \cdot g \cdot h)_C + (\cfrac{1}{2} \cdot m \cdot v^2)_C \]
@@ -215,7 +218,7 @@ return(
         \[v = 2\sqrt{15} m/s \]
       </MathJax>
       </MathJaxContext>
-      2) สามารถหาความเร็วที่จุด D เพราะพลังงานที่จุด A = พลังงานที่จุด D<br/>
+      2) สามารถหาความเร็วที่จุด D เพราะพลังงานรวมที่จุด A = พลังงานรวมที่จุด D<br/>
       เราจะพบว่าที่จุด A มีพลังงานศักย์โน้มถ่วง (มีความสูง) และจุด D มีแค่พลังงานจลน์ (มีความเร็ว)<br/>
       <MathJaxContext>
       <MathJax>\[(m \cdot g \cdot h)_A = (\cfrac{1}{2} \cdot m \cdot v^2)_D \]
@@ -232,7 +235,7 @@ return(
 </div>
 
 <div className="split QuestionAnswer"> 
-  <div className="LabNumber">Introducing Work</div>
+  <div className="LabNumber">Energy Transformed</div>
   <div className="ProgessBar"><progress value="20" max="100"></progress></div>
   <div className="Question">กดปุ่มสีเขียว เพื่อไปหน้าต่อไป</div>
   <div className="AnswerList">
@@ -250,33 +253,32 @@ return(
   <div className="split Index">
 <div className="LabName">กฎการอนุรักษ์พลังงาน</div>
 <div className="LabInfo">เรามาทดสอบความเข้าใจกันครับ <br/> <br/>
-  จากรูปวัตถุมีมวล 1 kg  เคลื่อนที่จากจุด A ไป จุด E โดยทางพื้นผิวไม่มีแรงเสียดทาน <br/>จงหาความเร็ววัตถุที่จุด B และ E
-<br/><br/><br/>********รูปรูปรูปรูปรูปรูป*
-
+  จากรูปวัตถุมีมวล 1 kg  เคลื่อนที่จากจุด A ไป จุด E โดยทางพื้นผิวไม่มีแรงเสียดทาน <br/>จงหาความเร็ววัตถุที่จุด B (ระหว่าง DE มีสัมประสิทธิ์แรงเสียดทาน = 0.1)
 </div> 
+<img className='LabImg' id='img' alt ="LabImg"src="https://firebasestorage.googleapis.com/v0/b/keep-curious.appspot.com/o/Work%26Energy%2FEp1%2FEp1P5.png?alt=media&token=44b39353-448b-4eb0-8655-3277af1ecc80" />
 <div div className="FooterSpace"></div>
  <div className="Footer">Curious Project</div>
  <div div className="FooterSpace"></div>
 </div>
 
 <div className="split QuestionAnswer"> 
-      <div className="LabNumber">Introducing Work</div>
+      <div className="LabNumber">Energy Transformed</div>
       <div className="ProgessBar"><progress value="40" max="100"></progress></div>
       <div className="Question"></div>
       <div className="AnswerList">
-      <label className="container">5 m/s , 5 m/s
+      <label className="container">5 m/s
           <input type="checkbox" id="Answer1" />
           <span className="checkmark"></span>
         </label>
-        <label className="container">5√12 m/s , 2√15 m/s
+        <label className="container">5√12 m/s
           <input type="checkbox" id="Answer2"/>
           <span className="checkmark"></span>
         </label>
-        <label className="container">2√15 m/s ,5√12 m/s
+        <label className="container">2√15 m/s
           <input type="checkbox" id="Answer3"/>
           <span className="checkmark"></span>
         </label>
-        <label className="container">10 m/s , 10 m/s
+        <label className="container">10 m/s
           <input type="checkbox" id="Answer4" />
           <span className="checkmark" ></span>
         </label>
@@ -295,36 +297,34 @@ function Page3Answered (){
     {AlertState.current === 1? <CorrectAlert/> : null}
     {AlertState.current === 2? <IncorrectAlert/> : null}
     <div className="split Index">
-  <div className="LabName">กฎการอนุรักษ์พลังงาน</div>
-  <div className="LabInfo">เรามาทดสอบความเข้าใจกันครับ <br/> <br/>จากรูปวัตถุมีมวล 1 kg  เคลื่อนที่จากจุด A ไป จุด E โดยทางพื้นผิวไม่มีแรงเสียดทาน <br/>จงหาความเร็ววัตถุที่จุด B และ E
-
-
-<br/><br/><br/>********รูปรูปรูปรูปรูปรูป*
-  </div> 
- 
-   <div div className="FooterSpace"></div>
-   <div className="Footer">Curious Project</div>
-   <div div className="FooterSpace"></div>
-  </div>
+<div className="LabName">กฎการอนุรักษ์พลังงาน</div>
+<div className="LabInfo">เรามาทดสอบความเข้าใจกันครับ <br/> <br/>
+  จากรูปวัตถุมีมวล 1 kg  เคลื่อนที่จากจุด A ไป จุด E โดยทางพื้นผิวไม่มีแรงเสียดทาน <br/>จงหาความเร็ววัตถุที่จุด B(ระหว่าง DE มีสัมประสิทธิ์แรงเสียดทาน = 0.1)
+</div> 
+<img className='LabImg' id='img' alt ="LabImg"src="https://firebasestorage.googleapis.com/v0/b/keep-curious.appspot.com/o/Work%26Energy%2FEp1%2FEp1P5.png?alt=media&token=44b39353-448b-4eb0-8655-3277af1ecc80" />
+<div div className="FooterSpace"></div>
+ <div className="Footer">Curious Project</div>
+ <div div className="FooterSpace"></div>
+</div>
   
   <div className="split QuestionAnswer"> 
-    <div className="LabNumber">Introducing Work</div>
+    <div className="LabNumber">Energy Transformed</div>
     <div className="ProgessBar"><progress value="40" max="100"></progress></div>
     <div className="Question"></div>
     <div className="AnswerList">
-    <label className="container">5 m/s , 5 m/s
+    <label className="container">5 m/s
         <input type="checkbox" id="Answer1" disabled/>
         <span className="checkmark"></span>
       </label>
-      <label className="container">5√12 m/s , 2√15 m/s
+      <label className="container">5√12 m/s
         <input type="checkbox" id="Answer2"disabled/>
         <span className="checkmark"></span>
       </label>
-      <label className="container">2√15 m/s ,5√12 m/s
+      <label className="container">2√15 m/s
         <input type="checkbox" id="Answer3"disabled/>
         <span className="checkmark"></span>
       </label>
-      <label className="container">10 m/s , 10 m/s
+      <label className="container">10 m/s
         <input type="checkbox" id="Answer4" disabled checked />
         <span className="checkmark" style={{backgroundColor : "rgb(var(--primary-color))"}}></span>
       </label>
@@ -344,16 +344,16 @@ function Page3Answered (){
       <div className="split Index">
     <div className="LabName">กฎการอนุรักษ์พลังงาน</div>
     <div className="LabInfo">ยิงจรวดขวดน้ำขึ้นด้วยความเร็ว 100 m/s ขึ้นในแนวดิ่ง จงหาว่าจรวดขวดน้ำจะขึ้นไปสูงสุดเท่าไร(ที่จุดสูงสุดความเร็วแนวดิ่ง = 0 เสมอ )
-
-    <br/><br/><br/>********ใส่รูปจ้า*
     </div> 
+    <img className='LabImg' id='img' alt ="LabImg"src="https://firebasestorage.googleapis.com/v0/b/keep-curious.appspot.com/o/NoImg1.png?alt=media&token=a3f3febd-e203-42e5-a018-7b7423a7a987" />
      <div div className="FooterSpace"></div>
+     <div className="Footer">Image by &nbsp;<a href='https://www.canva.com/media/MAEcRLcslH4'>sketchify</a></div>
      <div className="Footer">Curious Project</div>
      <div div className="FooterSpace"></div>
     </div>
     
     <div className="split QuestionAnswer"> 
-      <div className="LabNumber">Introducing Work</div>
+      <div className="LabNumber">Energy Transformed</div>
       <div className="ProgessBar"><progress value="60" max="100"></progress></div>
       <div className="Question"></div>
       <div className="AnswerList">
@@ -389,20 +389,19 @@ function Page3Answered (){
         <div>
     {AlertState.current === 1? <CorrectAlert/> : null}
     {AlertState.current === 2? <IncorrectAlert/> : null}
-        <div className="split Index">
-      <div className="LabName">กฎการอนุรักษ์พลังงาน</div>
-      <div className="LabInfo">
-      ยิงจรวดขวดน้ำขึ้นด้วยความเร็ว 100 m/s ขึ้นในแนวดิ่ง จงหาว่าจรวดขวดน้ำจะขึ้นไปสูงสุดเท่าไร(ที่จุดสูงสุดความเร็วแนวดิ่ง = 0 เสมอ )
-    <br/><br/><br/>********ใส่รูปจ้า*
-      </div> 
-     
-       <div div className="FooterSpace"></div>
-       <div className="Footer">Curious Project</div>
-       <div div className="FooterSpace"></div>
-      </div>
+    <div className="split Index">
+    <div className="LabName">กฎการอนุรักษ์พลังงาน</div>
+    <div className="LabInfo">ยิงจรวดขวดน้ำขึ้นด้วยความเร็ว 100 m/s ขึ้นในแนวดิ่ง จงหาว่าจรวดขวดน้ำจะขึ้นไปสูงสุดเท่าไร(ที่จุดสูงสุดความเร็วแนวดิ่ง = 0 เสมอ )
+    </div> 
+    <img className='LabImg' id='img' alt ="LabImg"src="https://firebasestorage.googleapis.com/v0/b/keep-curious.appspot.com/o/NoImg1.png?alt=media&token=a3f3febd-e203-42e5-a018-7b7423a7a987" />
+     <div div className="FooterSpace"></div>
+     <div className="Footer">Image by &nbsp;<a href='https://www.canva.com/media/MAEcRLcslH4'>sketchify</a></div>
+     <div className="Footer">Curious Project</div>
+     <div div className="FooterSpace"></div>
+    </div>
       
       <div className="split QuestionAnswer"> 
-        <div className="LabNumber">Introducing Work</div>
+        <div className="LabNumber">Energy Transformed</div>
         <div className="ProgessBar"><progress value="60" max="100"></progress></div>
         <div className="Question"></div>
         <div className="AnswerList">
@@ -438,16 +437,16 @@ function Page5 (){
           <div className="split Index">
         <div className="LabName">กฎการอนุรักษ์พลังงาน</div>
         <div className="LabInfo">ยิงธนูที่มีค่านิจสปริง 400 N/m ดึงสายธนู 10 cm ยิงลูกธนู 10 g ออกไป จงหาความเร็วของลูกธนูตอนออกจากคันธนู
-
-        <br/><br/><br/>********รูปรูปรูปรูปรูปรูป*
         </div> 
+        <img className='LabImg' id='img' alt ="LabImg"src="https://firebasestorage.googleapis.com/v0/b/keep-curious.appspot.com/o/NoImg2.png?alt=media&token=a7abff26-88d5-4523-809f-04288e4bddf5" />
          <div div className="FooterSpace"></div>
+         <div className="Footer">Image by &nbsp;<a href='https://www.canva.com/media/MADohmFXfv0'> iconsy</a></div>
          <div className="Footer">Curious Project</div>
          <div div className="FooterSpace"></div>
         </div>
         
         <div className="split QuestionAnswer"> 
-        <div className="LabNumber">Introducing Work</div>
+        <div className="LabNumber">Energy Transformed</div>
         <div className="ProgessBar"><progress value="80" max="100"></progress></div>
         <div className="Question"></div>
         <div className="AnswerList">
@@ -479,20 +478,19 @@ function Page5 (){
         <div>
     {AlertState.current === 1? <CorrectAlert/> : null}
     {AlertState.current === 2? <IncorrectAlert/> : null}
-        <div className="split Index">
-      <div className="LabName">กฎการอนุรักษ์พลังงาน</div>
-      <div className="LabInfo">ยิงธนูที่มีค่านิจสปริง 400 N/m ดึงสายธนู 10 cm ยิงลูกธนู 10 g ออกไป จงหาความเร็วของลูกธนูตอนออกจากคันธนู
-
-<br/><br/><br/>********รูปรูปรูปรูปรูปรูป*
-      </div> 
-     
-       <div div className="FooterSpace"></div>
-       <div className="Footer">Curious Project</div>
-       <div div className="FooterSpace"></div>
-      </div>
+    <div className="split Index">
+        <div className="LabName">กฎการอนุรักษ์พลังงาน</div>
+        <div className="LabInfo">ยิงธนูที่มีค่านิจสปริง 400 N/m ดึงสายธนู 10 cm ยิงลูกธนู 10 g ออกไป จงหาความเร็วของลูกธนูตอนออกจากคันธนู
+        </div> 
+        <img className='LabImg' id='img' alt ="LabImg"src="https://firebasestorage.googleapis.com/v0/b/keep-curious.appspot.com/o/NoImg2.png?alt=media&token=a7abff26-88d5-4523-809f-04288e4bddf5" />
+         <div div className="FooterSpace"></div>
+         <div className="Footer">Image by &nbsp;<a href='https://www.canva.com/media/MADohmFXfv0'> iconsy</a></div>
+         <div className="Footer">Curious Project</div>
+         <div div className="FooterSpace"></div>
+        </div>
       
       <div className="split QuestionAnswer"> 
-        <div className="LabNumber">Introducing Work</div>
+        <div className="LabNumber">Energy Transformed</div>
         <div className="ProgessBar"><progress value="80" max="100"></progress></div>
         <div className="Question"></div>
         <div className="AnswerList">
